e59b45761a6704afc04c844c6797ee41c664c6e1
TalhaProgrammer92 <talha.code92@gmail.com>
2025-02-16 07:24:42 +0500

    Update Tic-Tac-Toe

--- Tic Tac Game/program.cpp
+++ Tic Tac Game/program.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+/////////////
+// Colors
+/////////////
+class Color{
+public:
+    static const string Reset;
+    class ForeGround{
+        public:
+            static const string Red;
+            static const string Green;
+            static const string Yellow;
+            static const string Blue;
+            static const string Cyan;
+    };
+};
+
+// Reset
+const string Color::Reset = "\033[0m";
+
+// ForeGround Colors
+const string Color::ForeGround::Red = "\033[91m";
+const string Color::ForeGround::Green = "\033[92m";
+const string Color::ForeGround::Yellow = "\033[93m";
+const string Color::ForeGround::Blue = "\033[94m";
+const string Color::ForeGround::Cyan = "\033[96m";
+
 int main()
 {
-    cout << "Welcome to Tic Tac Game ☻" << endl;
+    string message = "Welcome to Tic Tac Game ☻";
+    cout << Color::ForeGround::Green + message + Color::Reset << endl;
 }
