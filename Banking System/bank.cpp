813a452f700bda5cc5fce54d6a4aa02734faf387
Talha Ahmad <talha.code92@gmail.com>
2025-02-24 18:32:57 +0500

    Update bank

--- Banking System/bank.cpp
+++ Banking System/bank.cpp
@@ -2,28 +2,30 @@
 
 using namespace std;
 
-// ? Global variables
+//? Global variables
 int accountNumber = 0;
 
 ////////////////////////
 //? Account class
 ////////////////////////
-class Account {
+class Account
+{
     //* Attributes
     int id;
     double balance;
     string name;
 
-    public:
+public:
     //* Constructor
-    Account(string name, double balance) {
+    Account(string name, double balance)
+    {
         this->name = name;
         this->balance = balance;
-        this->id = accountNumber++;
+        this->id = ++accountNumber;
     }
 };
 
 int main()
 {
-    
+    Account account1("Talha Ahmad", 15000), account2("Areeb", 20000);
 }
