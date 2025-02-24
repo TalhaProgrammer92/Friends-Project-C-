589aa69434e756943988fc33471d3d57ba8d5650
Talha Ahmad <talha.code92@gmail.com>
2025-02-24 17:07:35 +0500

    Updated Tic Tac

--- Tic Tac Game/program.cpp
+++ Tic Tac Game/program.cpp
@@ -3,16 +3,44 @@
 using namespace std;
 
 //////////////////////////
-// Foreground Colors
+//? Foreground Colors
 //////////////////////////
 const string RESET = "\033[0m";
 const string GREEN = "\033[32m";      /* Green */
 const string YELLOW = "\033[33m";     /* Yellow */
 const string BLUE = "\033[34m";       /* Blue */
+const string MAGENTA = "\033[35m";    /* Magenta */
+const string CYAN = "\033[36m";       /* Cyan */
 
 
+////////////////////////////
+//? Functions (Prototypes)
+////////////////////////////
+void printColor(string text, const string color);
+void printColor(char character, const string color);
+
+
+////////////////////////////
+//? Main Function
+////////////////////////////
 int main()
 {
     string message = "Welcome to Tic Tac Game";
     cout << GREEN << message << RESET << endl;
+}
+
+
+////////////////////////////
+//? Functions (Definition)
+////////////////////////////
+//* Print text (string) with color
+void printColor(string text, const string color)
+{
+    cout << color << text << RESET;
+}
+
+//* Print text (char) with color
+void printColor(char character, const string color)
+{
+    cout << color << character << RESET;
 }
