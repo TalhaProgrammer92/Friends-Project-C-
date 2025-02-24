0f70e3875891cff2e87d6c060d8a86f8a896926b
Talha Ahmad <talha.code92@gmail.com>
2025-02-24 17:51:57 +0500

    Added Account class in bank file

--- Banking System/bank.cpp
+++ Banking System/bank.cpp
@@ -5,6 +5,17 @@ using namespace std;
 // ? Global variables
 int accountNumber = 0;
 
+////////////////
+// Account
+////////////////
+class TAccount{
+    //* Variables
+    private:
+        int id;
+        string name;
+        double balance;
+};
+
 int main()
 {
     
