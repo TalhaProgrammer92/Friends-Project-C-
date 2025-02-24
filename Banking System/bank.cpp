208735f325718497848b66e8ead366622c3de5a2
Talha Ahmad <talha.code92@gmail.com>
2025-02-24 17:53:56 +0500

    Added Account class in Bank Project file

--- Banking System/bank.cpp
+++ Banking System/bank.cpp
@@ -14,9 +14,49 @@ class TAccount{
         int id;
         string name;
         double balance;
+    public:
+        //* Constructor
+        TAccount(string name, double balance){
+            this->id = ++accountNumber;
+            this->name = name;
+            this->balance = balance;
+        }
+        //* Getters
+        int getId(){
+            return this->id;
+        }
+        string getName(){
+            return this->name;
+        }
+        double getBalance(){
+            return this->balance;
+        }
+        //* Setters
+        void setName(string name){
+            this->name = name;
+        }
+        void setBalance(double balance){
+            this->balance = balance;
+        }
+        //* Methods
+        void deposit(double amount){
+            this->balance += amount;
+        }
+        void withdraw(double amount){
+            if(this->balance - amount < 0){
+                cout << "Insufficient funds" << endl;
+            }else{
+                this->balance -= amount;
+            }
+        }
+        void display(){
+            cout << "Account ID: " << this->id << endl;
+            cout << "Name: " << this->name << endl;
+            cout << "Balance: " << this->balance << endl;
+        }
 };
 
 int main()
 {
-    
+    TAccount talha_account("Talha", 5000);
 }
