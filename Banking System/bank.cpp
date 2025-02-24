1c89f7556918afc8805275663e8b4b7cb7a87341
Talha Ahmad <talha.code92@gmail.com>
2025-02-24 18:27:26 +0500

    Update Bank

--- Banking System/bank.cpp
+++ Banking System/bank.cpp
@@ -4,64 +4,15 @@ using namespace std;
 
 // ? Global variables
 int accountNumber = 0;
+
+////////////////////////
+//? Account class
+////////////////////////
 class Account {
     int id, balence;
-}
-
-////////////////
-// Account
-////////////////
-class Account{
-    //* Variables
-    private:
-        int id;
-        string name;
-        double balance;
-    public:
-        //* Constructor
-        Account(string name, double balance){
-            this->id = ++accountNumber;
-            this->name = name;
-            this->balance = balance;
-        }
-        //* Getters
-        int getId(){
-            return this->id;
-        }
-        string getName(){
-            return this->name;
-        }
-        double getBalance(){
-            return this->balance;
-        }
-        //* Setters
-        void setName(string name){
-            this->name = name;
-        }
-        void setBalance(double balance){
-            this->balance = balance;
-        }
-        //* Methods
-        void deposit(double amount){
-            this->balance += amount;
-        }
-        void withdraw(double amount){
-            if(this->balance - amount < 0){
-                cout << "Insufficient funds" << endl;
-            }else{
-                this->balance -= amount;
-            }
-        }
-        void display(){
-            cout << "Account ID: " << this->id << endl;
-            cout << "Name: " << this->name << endl;
-            cout << "Balance: " << this->balance << endl;
-        }
 };
 
 int main()
 {
-    Account talha_account("Talha", 5000);
-    talha_account.deposit(500);
-    talha_account.display();
+    
 }
