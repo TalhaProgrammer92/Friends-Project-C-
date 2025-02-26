5332e5d2e1db047f8e3f63ea7127a5616cf2a7d9
TalhaProgrammer92 <talha.code92@gmail.com>
2025-02-26 21:35:11 +0500

    Created a Bank class to handle some basic operations like account creation, money transfer, etc.

--- Banking System/bank.cpp
+++ Banking System/bank.cpp
@@ -36,7 +36,6 @@ public:
     void display();
 };
 
-
 ////////////////////////
 //? Bank class
 ////////////////////////
@@ -45,10 +44,13 @@ class Bank
     //* Attributes
     vector<Account> accounts;
 
-    public:
+public:
     //* Methods
+    void createAccount();
+    void displayAllAccounts();
+    void transfer(Account from, Account to, double amount);
+    Account getAccount(int id);
 };
-
 
 ////////////////////////
 //? Main Function
@@ -56,17 +58,33 @@ class Bank
 int main()
 {
     //* Creating objects of Account class
-    Account account1("Talha Ahmad", 15000), account2("Areeb", 25000), account3("Raza", 20000);
+    Bank bank;
 
-    //* Diposit (to account1) and withdraw (from account2) cash
-    account1.deposit(account2.withdraw(5000));
-    
-    //* Displaying account's information
-    account1.display();
-    account2.display();
-    account3.display();
+    //* Local variables
+    int choice;
+    cout << "1- Create account, 2- Display all accounts, 3- Open Existing Account" << endl;
+    cin >> choice;
+
+    //* Switch case for user choice
+    switch (choice)
+    {
+    case 1:
+        bank.createAccount();
+        break;
+    case 2:
+        bank.displayAllAccounts();
+        break;
+    case 3:
+        int id;
+        cout << "Enter account id: ";
+        cin >> id;
+        Account account = bank.getAccount(id);
+        account.display();
+        break;
+    default:
+        cout << "Invalid choice" << endl;
+    }
 }
-
 
 ////////////////////////
 //? Account class
@@ -96,4 +114,50 @@ void Account::display()
     cout << "Name: " << name << endl;
     cout << "Balance: " << balance << endl;
     cout << endl;
+}
+
+////////////////////////
+//? Bank class
+////////////////////////
+
+//* Method - Create account
+void Bank::createAccount()
+{
+    //? Local variables
+    string name;
+    double balance;
+
+    //? Taking input from user
+    cout << "Enter name: ";
+    cin >> name;
+    cout << "Enter balance: ";
+    cin >> balance;
+
+    //? Creating account object and pushing it to accounts vector
+    Account account(name, balance);
+    accounts.push_back(account);
+}
+
+//* Method - Display all accounts
+void Bank::displayAllAccounts()
+{
+    //? Looping through all accounts and displaying them
+    for (Account account : accounts)
+        account.display();
+}
+
+//* Method - Transfer cash from one account to another
+void Bank::transfer(Account from, Account to, double amount)
+{
+    //? Withdrawing cash from one account and depositing it to another account
+    to.deposit(from.withdraw(amount));
+}
+
+//* Method - Get account by id
+Account Bank::getAccount(int id)
+{
+    //? Looping through all accounts and returning the account with given id
+    for (Account account : accounts)
+        if (account.getId() == id)
+            return account;
 }
