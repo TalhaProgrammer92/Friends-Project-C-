b1fa6233b2537a955eafe9db46dced153ce8745b
Talha Ahmad <talha.code92@gmail.com>
2025-02-28 10:01:09 +0500

    I've added this player class to handle players data in this game.

--- Tic Tac Game/program.cpp
+++ Tic Tac Game/program.cpp
@@ -6,12 +6,11 @@ using namespace std;
 //? Foreground Colors
 //////////////////////////
 const string RESET = "\033[0m";
-const string GREEN = "\033[32m";      /* Green */
-const string YELLOW = "\033[33m";     /* Yellow */
-const string BLUE = "\033[34m";       /* Blue */
-const string MAGENTA = "\033[35m";    /* Magenta */
-const string CYAN = "\033[36m";       /* Cyan */
-
+const string GREEN = "\033[32m";   /* Green */
+const string YELLOW = "\033[33m";  /* Yellow */
+const string BLUE = "\033[34m";    /* Blue */
+const string MAGENTA = "\033[35m"; /* Magenta */
+const string CYAN = "\033[36m";    /* Cyan */
 
 ////////////////////////////
 //? Functions (Prototypes)
@@ -19,16 +18,49 @@ const string CYAN = "\033[36m";       /* Cyan */
 void printColor(string text, const string color);
 void printColor(char character, const string color);
 
+////////////////////////////
+//? Player Class
+////////////////////////////
+class Player
+{
+    string name;
+    char sign;
+
+public:
+    Player() {}
+    Player(string name, char sign)
+    {
+        this->name = name;
+        this->sign = sign;
+    }
+    string getName()
+    {
+        return name;
+    }
+    char getSign()
+    {
+        return sign;
+    }
+    void display()
+    {
+        printColor("Name:\t", CYAN);
+        printColor(name, YELLOW);
+        cout << endl;
+        printColor("Sign:\t", CYAN);
+        printColor(sign, YELLOW);
+        cout << endl;
+    }
+};
+
 
 ////////////////////////////
 //? Main Function
 ////////////////////////////
 int main()
 {
-    printColor("Hello World!", MAGENTA);
-    cout << endl;
+    Player p("Talha Ahmad", 'T');
+    p.display();
 }
-
 
 ////////////////////////////
 //? Functions (Definition)
