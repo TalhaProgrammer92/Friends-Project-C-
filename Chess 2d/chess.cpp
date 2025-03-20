9c6b5fd6eb95d8720e38ab6d1002ef7efce2c382
Talha Ahmad <talha.code92@gmail.com>
2025-03-20 15:54:42 +0500

    Added position class in chess game file

--- Chess 2d/chess.cpp
+++ Chess 2d/chess.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
-
-/*
-WHITE:	♚ ♛ ♜ ♝ ♞ ♟
-BLACK:	♔ ♕ ♖ ♗ ♘ ♙
-
-'black' : {
-    'king' : '\u2654',
-    'queen' : '\u2655',
-    'rook' : '\u2656',
-    'bishop' : '\u2657',
-    'knight' : '\u2658',
-    'pawn' : '\u2659'
-},
-'white' : {
-    'king' : '\u265A',
-    'queen' : '\u265B',
-    'rook' : '\u265C',
-    'bishop' : '\u265D',
-    'knight' : '\u265E',
-    'pawn' : '\u265F'
-}
-*/
 
 ///////////////
 //  Unicode  //
@@ -62,6 +39,31 @@ const string White::bishop = "\u265D";
 const string White::knight = "\u265E";
 const string White::pawn = "\u265F";
 
+//////////////
+// POSITION //
+//////////////
+class Position
+{
+private:
+    //* Variables
+    int row, column;
+
+public:
+    //* Constructor
+    Position(int row = 0, int column = 0) : row(row), column(column) {}
+
+    //* Getters and Setters
+    int getRow();
+    int getColumn();
+    void setRow(int row);
+    void setColumn(int column);
+
+    //* Set both
+    void setBoth(int row, int column);
+
+    //* Display
+    void display();
+};
 
 /////////////
 //  MAIN   //
@@ -69,7 +71,41 @@ const string White::pawn = "\u265F";
 int main()
 {
     // Set console code page to UTF-8
-    SetConsoleOutputCP(CP_UTF8);
+    // SetConsoleOutputCP(CP_UTF8);
 
     cout << Black::king << ' ' << White::king << endl;
+}
+
+//////////////
+// POSITION //
+//////////////
+int Position::getRow()
+{
+    return row;
+}
+
+int Position::getColumn()
+{
+    return column;
+}
+
+void Position::setRow(int row)
+{
+    this->row = row;
+}
+
+void Position::setColumn(int column)
+{
+    this->column = column;
+}
+
+void Position::setBoth(int row, int column)
+{
+    this->row = row;
+    this->column = column;
+}
+
+void Position::display()
+{
+    cout << '(' << row << ", " << column << ')';
 }
