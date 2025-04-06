d8002e5094b8527526879a45d9be7f72ef855977
Talha Ahmad <talha.code92@gmail.com>
2025-04-06 23:40:31 +0500

    Update Chess

--- Chess 2d/chess.cpp
+++ Chess 2d/chess.cpp
@@ -7,25 +7,31 @@ using namespace std;
 //////////////////////////
 // UNICODE CHESS PIECES //
 //////////////////////////
-#define WHITE_KING L"\u2654"
-#define WHITE_QUEEN L"\u2655"
-#define WHITE_ROOK L"\u2656"
-#define WHITE_BISHOP L"\u2657"
-#define WHITE_KNIGHT L"\u2658"
-#define WHITE_PAWN L"\u2659"
 
-#define BLACK_KING L"\u265A"
-#define BLACK_QUEEN L"\u265B"
-#define BLACK_ROOK L"\u265C"
-#define BLACK_BISHOP L"\u265D"
-#define BLACK_KNIGHT L"\u265E"
-#define BLACK_PAWN L"\u265F"
+//! Important for rendering chess pieces in the console
+#define WHITE_KING L"\u2654"    //? ♔
+#define WHITE_QUEEN L"\u2655"   //? ♕
+#define WHITE_ROOK L"\u2656"    //? ♖
+#define WHITE_BISHOP L"\u2657"  //? ♗
+#define WHITE_KNIGHT L"\u2658"  //? ♘
+#define WHITE_PAWN L"\u2659"    //? ♙
 
-#define FILLED_CIRCLE L'\u25CF'
-#define HOLLOW_CIRCLE L'\u25CB'
+#define BLACK_KING L"\u265A"    //? ♚
+#define BLACK_QUEEN L"\u265B"   //? ♛
+#define BLACK_ROOK L"\u265C"    //? ♜
+#define BLACK_BISHOP L"\u265D"  //? ♝
+#define BLACK_KNIGHT L"\u265E"  //? ♞
+#define BLACK_PAWN L"\u265F"    //? ♟
 
-//* const wchar_t* white_rook = WHITE_ROOK;
-//* wcout << white_rook << endl;
+#define FILLED_CIRCLE L'\u25CF' //? ●
+#define HOLLOW_CIRCLE L'\u25CB' //? ○
+
+/*
+ ! Example of displaying Unicode characters:
+
+ ? const wchar_t* white_rook = WHITE_ROOK;
+ ? wcout << white_rook << endl;
+*/
 
 ///////////////
 // Box Class //
@@ -37,8 +43,8 @@ public:
     wchar_t item;
 
     //* Constructor
-    Box() {}                //! Default constructor
-    Box(wchar_t item);      //! Constructor with item (parameter)
+    Box() {}           //! Default constructor
+    Box(wchar_t item); //! Constructor with item (parameter)
 
     //* Display the box with the item
     void display();
@@ -63,18 +69,29 @@ public:
     void display();
 };
 
+///////////////
+// Functions //
+///////////////
+//* Set console to Unicode mode
+void setUnicodeMode();
+
 /////////////////
 // Entry Point //
 /////////////////
 int main()
 {
-    //! Set console to Unicode mode
-    _setmode(_fileno(stdout), _O_U16TEXT);
-    _setmode(_fileno(stdin), _O_U16TEXT);
-    _setmode(_fileno(stderr), _O_U16TEXT);
+    //* Set console to Unicode mode
+    setUnicodeMode();
 
-    Board board;        //? Create a board object
-    board.display();    //? Clear the board
+    Board board;     //! Create a board object
+    board.display(); //! Clear the board
+
+    //* Dislpay chess pieces
+    wcout << endl;
+    
+    wcout << WHITE_KING << " " << WHITE_QUEEN << " " << WHITE_ROOK << " " << WHITE_BISHOP << " " << WHITE_KNIGHT << " " << WHITE_PAWN << endl;
+    
+    wcout << BLACK_KING << " " << BLACK_QUEEN << " " << BLACK_ROOK << " " << BLACK_BISHOP << " " << BLACK_KNIGHT << " " << BLACK_PAWN << endl;
 
     return 0;
 }
@@ -117,7 +134,7 @@ void Board::clear()
                 //! Even column
                 if (j % 2 == 0)
                     boxes[i][j] = Box(HOLLOW_CIRCLE);
-                
+
                 //! Odd column
                 else
                     boxes[i][j] = Box(FILLED_CIRCLE);
@@ -126,8 +143,11 @@ void Board::clear()
             //! Odd row
             else
             {
+                //! Even column
                 if (j % 2 == 0)
                     boxes[i][j] = Box(FILLED_CIRCLE);
+
+                //! Odd column
                 else
                     boxes[i][j] = Box(HOLLOW_CIRCLE);
             }
@@ -143,8 +163,24 @@ void Board::display()
         for (int j = 0; j < 8; j++)
         {
             boxes[i][j].display();
-            wcout << " "; // Space between boxes
+            wcout << " ";
         }
-        wcout << endl; // New line after each row
+        wcout << endl;
     }
+}
+
+///////////////
+// Functions //
+///////////////
+//* Set console to Unicode mode
+void setUnicodeMode()
+{
+    //! To display Unicode characters in the console
+    _setmode(_fileno(stdout), _O_U16TEXT);
+
+    //! To read Unicode characters from the console
+    _setmode(_fileno(stdin), _O_U16TEXT);
+
+    //! To display Unicode characters in the console (error output)
+    _setmode(_fileno(stderr), _O_U16TEXT);
 }
