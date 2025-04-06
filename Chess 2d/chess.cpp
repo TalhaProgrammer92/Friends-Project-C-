2e7ea326bfd2b6ae673f13816bca35b588170cc4
Talha Ahmad <talha.code92@gmail.com>
2025-04-06 13:00:44 +0500

    Update Chess

--- Chess 2d/chess.cpp
+++ Chess 2d/chess.cpp
@@ -2,30 +2,72 @@
 #include <vector>
 #include <windows.h>
 
+//! This program contain bugs and is not fully functional. It is a work in progress and should not be used in production.
+//! The code is not complete and may not compile or run as expected. Use at your own risk.
+
 using namespace std;
 
 //////////////////////
 //  Unicode Pieces  //
 //////////////////////
-enum class BlackPiece
-{
-    king = 0x2654,
-    queen = 0x2655,
-    rook = 0x2656,
-    bishop = 0x2657,
-    knight = 0x2658,
-    pawn = 0x2659
-};
+/*
+// Unicode Chess Piece Macros
+#define WHITE_KING   L"\u2654"
+#define WHITE_QUEEN  L"\u2655"
+#define WHITE_ROOK   L"\u2656"
+#define WHITE_BISHOP L"\u2657"
+#define WHITE_KNIGHT L"\u2658"
+#define WHITE_PAWN   L"\u2659"
+#define BLACK_KING   L"\u265A"
+#define BLACK_QUEEN  L"\u265B"
+#define BLACK_ROOK   L"\u265C"
+#define BLACK_BISHOP L"\u265D"
+#define BLACK_KNIGHT L"\u265E"
+#define BLACK_PAWN   L"\u265F"
 
-enum class WhitePiece
-{
-    king = 0x265A,
-    queen = 0x265B,
-    rook = 0x265C,
-    bishop = 0x265D,
-    knight = 0x265E,
-    pawn = 0x265F
-};
+// Example usage:
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
+int main() {
+    // Set console to Unicode mode
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    
+    // Store in variable
+    const wchar_t* white_rook = WHITE_ROOK;
+
+    std::wcout << L"Chess pieces:\n";
+    std::wcout << WHITE_KING << L" White King\n";
+    std::wcout << WHITE_QUEEN << L" White Queen\n";
+    std::wcout << WHITE_ROOK << L" White Rook\n";
+    std::wcout << WHITE_BISHOP << L" White Bishop\n";
+    std::wcout << WHITE_KNIGHT << L" White Knight\n";
+    std::wcout << WHITE_PAWN << L" White Pawn\n";
+    std::wcout << BLACK_KING << L" Black King\n";
+    std::wcout << BLACK_QUEEN << L" Black Queen\n";
+    std::wcout << BLACK_ROOK << L" Black Rook\n";
+    std::wcout << BLACK_BISHOP << L" Black Bishop\n";
+    std::wcout << BLACK_KNIGHT << L" Black Knight\n";
+    std::wcout << BLACK_PAWN << L" Black Pawn\n";
+    
+    return 0;
+}
+
+Notes:
+1. The _setmode and _O_U16TEXT are Windows-specific. For Linux/macOS, you might need different setup.
+
+2. For cross-platform compatibility, you might want to wrap the console setup in platform-specific code.
+
+3. Make sure your console/terminal uses a font that supports these Unicode characters (most modern ones do).
+
+4. Notice there was a typo in WHITE_KNIGHT (should be WHITE_KNIGHT) which I've corrected in the example usage.
+
+5. For C++20 and later, you could also use char8_t and UTF-8 string literals.
+*/
+
+//* Set console to support Unicode characters
+void setUnicodeSupport();
 
 ////////////////////
 // POSITION CLASS //
@@ -164,10 +206,33 @@ public:
 /////////////
 int main()
 {
-    //! Set console code page to UTF-8
-    SetConsoleOutputCP(CP_UTF8);
+    //? Set console to support Unicode characters
+    setUnicodeSupport();
+
+    //? Chess piece - testing
+    cout << "White King: " << (char)WhitePiece::king << endl;
 
     return 0;
+}
+
+//* Set console to support Unicode characters
+void setUnicodeSupport()
+{
+    //? Set console code page to UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    //? Set console font to a font that supports Unicode characters
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.dwFontSize.X = 0;       //! Width of each character in the font
+    cfi.dwFontSize.Y = 16;      //! Height
+    cfi.FontFamily = FF_DONTCARE;       //! Font family
+    cfi.FontWeight = FW_NORMAL;         //! Font weight
+    cfi.nFont = 0;      //! Font number
+    wcscpy_s(cfi.FaceName, L"Consolas"); //! Font name
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0F);         //! Set text color to white
 }
 
 //////////////
